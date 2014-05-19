387899acbbfae327edd82f4f7f8e46562f5b97cd
Asif R Choudhury <choudhury.asif@gmail.com>
2014-05-19 14:08:55 -0500

    Asif & Cameron | Added mocha and a basic test (Cameron's first mocha test! Yay!)

---
+++ README.md
@@ -0,0 +1,2 @@
+#Selfie Quest!
+[![Build Status](https://drone.io/github.com/asifrc/selfiequest/status.png)](https://drone.io/github.com/asifrc/selfiequest/latest)
---
+++ test/firstTest.js
@@ -0,0 +1,8 @@
+var should = require('should');
+
+describe( "firstTest", function() {
+  it("should pass the basic test", function() {
+    var subject = "Hella World";
+    subject.should.equal("Hella World");
+  })
+})
