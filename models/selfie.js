269612e690348726b97b5834f10617c90b2be9ca
Asif R Choudhury <choudhury.asif@gmail.com>
2014-05-21 14:56:49 -0500

    Asif & Cameron | Extracted Selfie Model from the Route

---
+++ models/selfie.js
@@ -0,0 +1,55 @@
+var mongoose = require('mongoose');
+var multiparty = require('multiparty');
+var knox = require('knox');
+var MultiPartUpload = require('knox-mpu');
+
+/***************\
+ * Selfie Model *
+\***************/
+var Selfie = mongoose.model('Selfie', { path: String });
+
+var knoxSettings = {
+  key: process.env.AWS_ACCESS_KEY_ID,
+  secret: process.env.AWS_SECRET_ACCESS_KEY,
+  bucket: 'selfiequestdev'
+};
+
+// Upload selfie
+var uploadPhoto = function(req, res) {
+  var form = new multiparty.Form();
+
+  form.parse(req, function(err, fields, files) {
+    var selfie = files.photoFile[0];
+    // console.log(selfie);
+
+    var client = knox.createClient(knoxSettings);
+
+    var obj = {
+      client: client,
+      objectName: selfie.originalFilename, // Amazon S3 object name
+      file: selfie.path
+    };
+    var upload = new MultiPartUpload(obj, function(err, body) {
+      if (err) {
+        res.send("ERROR!!!! : " + err);
+      }
+      else
+      {
+        mongoose.connect(process.env.MONGO_DB);
+        var selfie = new Selfie({ path: body.Location });
+        selfie.save(function(err) {
+          if (err) {
+            res.send("Error updating database with selfie path: " + err);
+          }
+          else {
+            res.render('tag', {imgPath: selfie.path})
+          }
+        });
+      }
+    });
+  });
+}
+
+module.exports = {
+  uploadPhoto: uploadPhoto
+};
--- routes/selfies.js
+++ routes/selfies.js
@@ -1,50 +1,11 @@
 var router = require('express').Router();
-var multiparty = require('multiparty');
 var fs = require('fs');
 var path = require('path');
-var mongoose = require('mongoose');
 
-var knox = require('knox');
-var MultiPartUpload = require('knox-mpu');
+var selfie = require('../models/selfie')
 
 router.post('/tag', function(req, res) {
-	var form = new multiparty.Form();
-
-	form.parse(req, function(err, fields, files) {
-		var photo = files.photoFile[0];
-		// console.log(photo);
-
-    var client = knox.createClient({
-      key: process.env.AWS_ACCESS_KEY_ID,
-      secret: process.env.AWS_SECRET_ACCESS_KEY,
-      bucket: 'selfiequestdev'
-    });
-
-		var obj = {
-			client: client,
-			objectName: photo.originalFilename, // Amazon S3 object name
-			file: photo.path
-		};
-    var upload = new MultiPartUpload(obj, function(err, body) {
-      if (err) {
-        res.send("ERROR!!!! : " + err);
-      }
-      else
-      {
-				mongoose.connect(process.env.MONGO_DB);
-				var Photo = mongoose.model('Photo', { path: String });
-				var photo = new Photo({ path: body.Location });
-				photo.save(function(err) {
-					if (err) {
-						res.send("Error updating database with photo path: " + err);
-					}
-					else {
-						res.render('tag', {imgPath: photo.path})
-					}
-				});
-      }
-    });
-	});
+	selfie.uploadPhoto(req, res);
 });
 
 module.exports = router;
