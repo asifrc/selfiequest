dbd4e7e9ceecd41487fd67531920936c72b4af47
Asif R Choudhury <choudhury.asif@gmail.com>
2014-05-28 18:23:40 -0500

    Asif & Cameron | Switched Tag dropdown to populate list from an array

--- public/scripts/tag.js
+++ public/scripts/tag.js
@@ -5,7 +5,7 @@ var selectUser = function() {
   $(this).buttonMarkup({ icon: 'delete', iconpos: 'right'});
 
   $('#userList li a').off('click');
-  $(this).click(unselectUser);
+  $(this).on('click', unselectUser);
 };
 
 var unselectUser = function() {
@@ -14,10 +14,10 @@ var unselectUser = function() {
   $('#tagged').val("");
 
   $(this).off('click');
-  $('#userList li a').click(selectUser);
+  $('#userList li a').on('click', selectUser);
 };
 
-$('#userList li a').click(selectUser);
+// $('#userList').delegate('li a', 'click', selectUser);
 
 $('#saveButton').click(function() {
   if ($('#tagged').val() !== "") {
@@ -26,4 +26,18 @@ $('#saveButton').click(function() {
   else {
     $('#popup').popup('open');
   }
+});
+
+var users = ["Asif", "Cameron", "Pat", "Demo"];
+
+
+var usersHtml = function() {
+  return users.map(function(name) {
+    return '<li data-icon="false"><a href="#" data-value="'+name+'">'+name+'</a></li>'
+  });
+};
+
+$('#userList').on('filterablebeforefilter', function(e, data) {
+  $(this).html(usersHtml());
+  $('#userList li a').on('click', selectUser);
 });
