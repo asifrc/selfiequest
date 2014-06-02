3ad483f169513f81df0238296734601160148c9e
Asif R Choudhury <choudhury.asif@gmail.com>
2014-06-01 23:53:58 -0500

    Asif | Added methods to User model for getting Rank and Points, then passed it to view. Greeting is now fully dynamic.

--- models/user.js
+++ models/user.js
@@ -27,6 +27,7 @@ var getAllNamesAndIds = function(callback) {
       callback(err);
       return;
     }
+    
     var userNames = [];
     users.map(function(user) {
       var userObj = {
@@ -35,7 +36,59 @@ var getAllNamesAndIds = function(callback) {
       }
       userNames.push(userObj);
     });
+    
     callback(err, userNames);
+  });
+};
+
+var getAllSortedByRank = function(callback) {
+  User.find(function(err, users) {
+    if (err) {
+      callback(err);
+      return;
+    }
+    
+    //Sort Descending
+    users.sort(function(a, b) { return a.points < b.points; })
+    
+    callback(err, users);
+  });
+};
+
+var getAllWithRank = function(callback) {
+  getAllSortedByRank(function(err, users) {
+    if (err) {
+      callback(err);
+      return;
+    }
+    
+    users.map(function(user, rank) {
+      user.rank = rank + 1;
+      return user;
+    });
+    
+    callback(err, users);
+  });
+};
+
+var getRankFor = function(userId, callback) {
+  getAllWithRank(function(err, users) {
+    if (err) {
+      callback(err);
+      return;
+    }
+    
+    var foundUser = users.reduce(function(prev, curr) {
+      var foundUser = (curr._id == userId) ? curr : false;
+      return prev || foundUser;
+    }, false);
+    
+    if (!foundUser) {
+      callback("User not found in getRankFor!");
+      return;
+    }
+    
+    callback(err, foundUser);
   });
 };
 
@@ -43,5 +96,7 @@ module.exports = {
   create: create,
   findByToken: findByToken,
   getAllNamesAndIds: getAllNamesAndIds,
+  getAllWithRank: getAllWithRank,
+  getRankFor: getRankFor,
   Model: User
 };
--- routes/index.js
+++ routes/index.js
@@ -1,14 +1,31 @@
 var express = require('express');
 var router = express.Router();
 
+var User = require('../models/user');
+
+var getOrdinal = function(n) {
+   var s=["th","st","nd","rd"],
+       v=n%100;
+   return n+(s[(v-20)%10]||s[v]||s[0]);
+}
+
 /* GET home page. */
 router.get('/', function(req, res) {
-  var user = {
-    name: req.session.userName,
-    place: "4th",
-    points: 250
-  }
-  res.render('index', { title: 'Selfie Quest', user: user });
+  User.getRankFor(req.session.userId, function(err, rankedUser) {
+    if (err) {
+      res.render('error', { message: err, error: new Error(err) });
+      return;
+    }
+    
+    var user = {
+      name: rankedUser.name,
+      place: getOrdinal(rankedUser.rank),
+      points: rankedUser.points + " point"
+    }
+    user.points += (rankedUser.points != 1) ? "s" : "";
+    
+    res.render('index', { title: 'Selfie Quest', user: user });    
+  });
 });
 
 module.exports = router;
