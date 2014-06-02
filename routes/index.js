9683e852fda4e5c27d7d6691879198a54defb541
Asif R Choudhury <choudhury.asif@gmail.com>
2014-06-01 22:45:00 -0500

    Asif | Greeting Place and Points comes from router, but not yet calculated

--- routes/index.js
+++ routes/index.js
@@ -4,7 +4,9 @@ var router = express.Router();
 /* GET home page. */
 router.get('/', function(req, res) {
   var user = {
-    name: req.session.userName
+    name: req.session.userName,
+    place: "4th",
+    points: 250
   }
   res.render('index', { title: 'Selfie Quest', user: user });
 });
