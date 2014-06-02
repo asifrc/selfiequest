739f2288a9dc697fc9d077dfe1c7ed8e8252e1fd
Asif R Choudhury <choudhury.asif@gmail.com>
2014-06-02 13:31:56 -0500

    Asif & Cameron | Added view for delete pictures (Admin)

--- routes/admin.js
+++ routes/admin.js
@@ -1,14 +1,25 @@
 var express = require('express');
 var user = require('../models/user');
+var selfie = require('../models/selfie');
 var router = express.Router();
 
-router.get("/dev/createUser", function(req, res) {
+router.get('/dev/createUser', function(req, res) {
   res.render('createUser');
 });
 
-router.post("/dev/createUser", function(req, res) {
+router.post('/dev/createUser', function(req, res) {
   user.create(req.body.name, req.body.email, function(err) {
     res.render('createUser');
+  });
+});
+
+router.get('/admin/delete', function(req, res) {
+  selfie.findAllSelfies(function(err, selfies) {
+    if (err) {
+      res.render('error', { message: err, error: new Error(err) });
+      return;
+    }
+    res.render('deleteSelfie', { title: "Delete Selfies", selfies: selfies});
   });
 });
 
