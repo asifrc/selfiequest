0c2e12c7f80daebbc9d84bafcad4d368edd06737
crowshan <crowshanbin@gmail.com>
2014-06-04 15:21:03 -0500

    Cameron | Created scripts to populate the db from a csv
    tools/populateDbFromCsv.js: for a csv with thoughtWorker
    email,firstName,lastName
    tools/populateDbFromGuest.js: for a csv with guest
    email,firstName,lastName
    
    IMPORTANT: the User model has changed: it now has a firstName field, and
    authentication tokens for it are now generated with email + name instead
    of just email (some guests have duplicate emails)

---
+++ tools/populateDbFromCsv.js
@@ -0,0 +1,63 @@
+var mongoose = require('mongoose');
+var crypto = require('crypto');
+var config = require('../config');
+
+mongoose.connect(process.env.MONGO_DB);
+
+var User = mongoose.model('User', {
+  name: String,
+  firstName: String,
+  email: String,
+  token: String,
+  points: Number
+});
+
+var generateToken = function(key) {
+  var salt = config.security.salt;
+  return crypto.createHash('md5').update(key + salt).digest('hex');
+};
+
+var create = function(firstName, name, email) {
+  var user = new User({ firstName: firstName, name: name, email: email });
+  user.token = generateToken(email + name);
+  user.points = 0;
+
+  user.save();
+};
+
+var parseCsvLineForUserInfo = function(line) {
+  var userInfoMatcher = /^([^,]*),([^,]*),([^,]*)$/;
+  var userInfo = line.match(userInfoMatcher);
+
+  if (userInfo[0] === "") {
+    console.log("Couldn't parse line: " + line);
+  }
+  var nameAndEmail = {
+    email: userInfo[1],
+    firstName: userInfo[2],
+    name: userInfo[2] + " " + userInfo[3]
+  };
+
+  return nameAndEmail;
+};
+
+var getAndCreateUsersFromCsvFile = function(csvFile) {
+  var reader = require("buffered-reader");
+  var DataReader = reader.DataReader;
+  new DataReader (csvFile, { encoding: "utf8" })
+    .on ("error", function (error){
+        console.log ("error: " + error);
+    })
+    .on ("line", function (line){
+        console.log ("line: " + line);
+        var nameAndEmail = parseCsvLineForUserInfo(line);
+        console.log(nameAndEmail);
+        create(nameAndEmail.firstName, nameAndEmail.name, nameAndEmail.email);
+    })
+    .on ("end", function (){
+        console.log ("EOF");
+    })
+    .read ();
+};
+
+getAndCreateUsersFromCsvFile("./tools/thoughtWorkerList.csv");
---
+++ tools/populateDbFromGuestCsv.js
@@ -0,0 +1,64 @@
+var mongoose = require('mongoose');
+var crypto = require('crypto');
+var config = require('../config');
+
+mongoose.connect(process.env.MONGO_DB);
+
+var User = mongoose.model('User', {
+  name: String,
+  firstName: String,
+  email: String,
+  token: String,
+  points: Number
+});
+
+var generateToken = function(key) {
+  var salt = config.security.salt;
+  var key = crypto.createHash('md5').update(key + salt).digest('hex');
+  return key.substring(0,8);
+};
+
+var create = function(firstName, name, email) {
+  var user = new User({ firstName: firstName, name: name, email: email });
+  user.token = generateToken(email + name);
+  user.points = 0;
+
+  user.save();
+};
+
+var parseCsvLineForUserInfo = function(line) {
+  var userInfoMatcher = /^([^,]*),([^,]*),([^,]*)$/;
+  var userInfo = line.match(userInfoMatcher);
+
+  if (userInfo[0] === "") {
+    console.log("Couldn't parse line: " + line);
+  }
+  var nameAndEmail = {
+    email: userInfo[1],
+    firstName: userInfo[2],
+    name: userInfo[2] + " " + userInfo[3]
+  };
+
+  return nameAndEmail;
+};
+
+var getAndCreateUsersFromCsvFile = function(csvFile) {
+  var reader = require("buffered-reader");
+  var DataReader = reader.DataReader;
+  new DataReader (csvFile, { encoding: "utf8" })
+    .on ("error", function (error){
+        console.log ("error: " + error);
+    })
+    .on ("line", function (line){
+        console.log ("line: " + line);
+        var nameAndEmail = parseCsvLineForUserInfo(line);
+        console.log(nameAndEmail);
+        create(nameAndEmail.firstName, nameAndEmail.name, nameAndEmail.email);
+    })
+    .on ("end", function (){
+        console.log ("EOF");
+    })
+    .read ();
+};
+
+getAndCreateUsersFromCsvFile("./tools/guestList.csv");
