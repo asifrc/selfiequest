894861af6db681c15fffa5816229a29c1533f216
Asif R Choudhury <choudhury.asif@gmail.com>
2014-06-05 01:15:53 -0500

    Asif | Shortened the roundtrips to the db in target model by using mongoose.Schema.ObjectId in lieu of a .findById() call

--- models/target.js
+++ models/target.js
@@ -2,6 +2,8 @@ var mongoose = require('mongoose');
 var user = require('./user');
 var selfie = require('./selfie');
 
+var ObjectId = mongoose.Schema.ObjectId;
+
 var Target = mongoose.model('Target', {
   owner: {
     type: mongoose.Schema.Types.ObjectId,
@@ -17,52 +19,54 @@ var Target = mongoose.model('Target', {
 });
 
 
-var generateNextTarget = function(userId, callback, quantity) {
+var generateNextTarget = function(ownerId, callback, quantity, data) {
   quantity = (typeof quantity === "undefined") ? 1: quantity;
   // $1 Find every user
     // $2 Find every selfie i took or am tagged in
       // $3 Eliminate from $1 every 'other' user tagged in $2
-  user.Model.findById(userId, function(err, owner) {
-    user.Model.find(function(err, users) {
-      var potentialTargets = [];
-      users.map(function(user) {
-        potentialTargets[user._id] = user;
+  var owner = ObjectId(ownerId);
+  user.Model.find(function(err, users) {
+    var potentialTargets = [];
+    users.map(function(user) {
+      potentialTargets[user._id] = user;
+    });
+    
+    delete potentialTargets[ownerId];
+    
+    selfie.findSelfiesFor(ownerId, function(err, selfies) {
+      selfies.map(function(selfie) {
+        var otherUser = (selfie.owner == ownerId) ? selfie.tagged : selfie.owner;
+        delete potentialTargets[otherUser];
       });
-      
-      delete potentialTargets[userId];
-      
-      selfie.findSelfiesFor(userId, function(err, selfies) {
-        selfies.map(function(selfie) {
-          var otherUser = (selfie.owner == userId) ? selfie.tagged : selfie.owner;
-          delete potentialTargets[otherUser];
+      Target.find({ owner: owner}, function(err, existingTargets) {
+        existingTargets.map(function(xTarget) {
+          delete potentialTargets[xTarget.target];
         });
-        Target.find({ owner: owner}, function(err, existingTargets) {
-          existingTargets.map(function(xTarget) {
-            delete potentialTargets[xTarget.target];
-          });
-          var targets = [];
-          for (var target in potentialTargets) {
-            targets.push(potentialTargets[target]);
-          }
-          
-          newTargets = targets.slice(0, quantity);
-          
-          var counter = 0;
-          if (newTargets.length > 0) {
-            newTargets.map(function(targetUser) {
-              var newTarget = new Target({
-                owner: owner,
-                target: targetUser    
-              });
-              var cb = (counter++ === 0) ? callback : function() {};
-              newTarget.save(cb);
+        var targets = [];
+        for (var target in potentialTargets) {
+          targets.push(potentialTargets[target]);
+        }
+        
+        newTargets = targets.slice(0, quantity);
+        
+        var counter = 0;
+        if (newTargets.length > 0) {
+          newTargets.map(function(targetUser) {
+            var newTarget = new Target({
+              owner: owner,
+              target: targetUser    
             });
-          }
-          else {
-            callback();
-          }
-        });      
-      });
+            var cb = function() {};
+            if (counter++ === 0) {
+              cb = function(error) { callback(error, data); };
+            }
+            newTarget.save(cb);
+          });
+        }
+        else {
+          callback(err, data);
+        }
+      });      
     });
   });
 };
