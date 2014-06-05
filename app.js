24a740ed434a33376e1bee2f0cce9b21b79e92be
Julia Lee <julee@thoughtworks.com>
2014-06-05 10:03:14 -0500

    Julia & Asif | re-running test. added logging statements for SSL stuff

--- app.js
+++ app.js
@@ -15,7 +15,6 @@ var users = require('./routes/users');
 
 var app = express();
 
-// forcing SSL
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
@@ -32,15 +31,18 @@ app.use(expressSession({
 app.use(express.static(path.join(__dirname, 'public')));
 app.use(logger('dev'));
 
-// if (app.get('env') !== 'development') {
-app.use(function(req, res, next) {
-  if (req.headers['x-forwarded-proto'] !== 'https') {
-    return res.redirect("https://" + req.headers.host + req.url); 
-  } else {
-    return next();
-  }
-});
-// }
+console.log("ENV: " + app.get('env'));
+
+if (app.get('env') !== 'development') {
+  console.log("inside production");
+  app.use(function(req, res, next) {
+    if (req.headers['x-forwarded-proto'] !== 'https') {
+      return res.redirect("https://" + req.headers.host + req.url); 
+    } else {
+      return next();
+    }
+  });
+}
 
 app.use('/auth/:token', auth.authenticate);
 
