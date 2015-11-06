cd828b0e0a7e8f3fb6e529ef56d15f44c97cd69f
Asif R Choudhury <choudhury.asif@gmail.com>
2015-11-05 22:40:55 -0500

    Asif | Changed Config, and added environment variable for S3 bucket variable

--- config.js
+++ config.js
@@ -1,18 +1,29 @@
+var die = function(message) {
+  console.error(message);
+  throw new Error(message);
+};
+var requiredVar = function(varName) {
+  return process.env[varName] || die(varName + " is missing");
+}
+var optionalVar = function(varName, defaultValue) {
+  return process.env[varName] || defaultValue;
+};
+
 module.exports = {
   knox: {
       settings: {
-        key: process.env.AWS_ACCESS_KEY_ID,
-        secret: process.env.AWS_SECRET_ACCESS_KEY,
-        bucket: 'selfiequestdev'
+        key: requiredVar('AWS_ACCESS_KEY_ID'),
+        secret: requiredVar('AWS_SECRET_ACCESS_KEY'),
+        bucket: requiredVar('AWS_S3_BUCKET')
       }
   },
   aws: {
-    targetFolder: process.env.AWS_FOLDER || "default",
-    environment: process.env.ENVIRONMENT || "dev",
-    startGame: process.env.START_GAME || "false"
+    targetFolder: optionalVar('AWS_FOLDER', "default"),
+    environment: optionalVar('ENVIRONMENT',"dev"),
+    startGame: optionalVar('START_GAME', "false")
   },
   security: {
-    salt: process.env.SALT || "a3jcLj3kaB",
-    adminPassword: process.env.ADMIN_PASSWORD || "iceland"
+    salt: optionalVar('SALT', "a3jcLj3kaB"),
+    adminPassword: optionalVar('ADMIN_PASSWORD', "iceland")
   }
 };
